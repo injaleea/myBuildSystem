dbb5e82dd24fa323f8736fad5338fbca8e3c440d
Moopantakath Abdul Jaleel <abdul.jaleel@rfa.space>
2023-05-04 18:20:04 +0200

    All

--- vars/lvPipeline.groovy
+++ vars/lvPipeline.groovy
@@ -74,7 +74,7 @@ def call(lvProjectPath, lvBuildSpecName, lvVersion, lvBitness) {
 			echo 'env.CHANGE_ID'
 		
 			// If this change is a pull request, diff the VIs.
-			if (env.CHANGE_ID) {
+			if ${env.CHANGE_ID} {
 				stage ('Diff VIs'){
 					try {
 					timeout(time: 60, unit: 'MINUTES') {

784345cc43860af0564be679bfee854b4ec6b121
Moopantakath Abdul Jaleel <abdul.jaleel@rfa.space>
2023-05-05 10:59:51 +0200

    Added them all

--- vars/lvDiff.groovy
+++ vars/lvDiff.groovy
@@ -7,7 +7,7 @@ def call(lvVersion, lvBitness, accessToken) {
 
 	echo 'Running LabVIEW diff build between origin/main and this commit'
 
-	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${operationsDir}\\\\\" \"${diffDir}\\\\\" ${lvVersion} ${lvBitness} --target=origin/main"
+	bat "python -u \"${stepsDir}\\labview_diff.py\" \"${operationsDir}\\\\\" \"${diffDir}\\\\\" ${lvVersion} ${lvBitness} --target=origin/master"
     
 	bat "python -u \"${stepsDir}\\github_commenter.py\" --token=\"${accessToken}\" --pic-dir=\"${diffDir}\" --pull-req=\"8\" --info=\"${ORG_NAME}/${repo}/8\" --pic-repo=\"${ORG_NAME}/${PIC_REPO}\""
 }
